bc1bd401b44eb0f7a56f6cb09188a7fac5def5ac
Oleg G.Kapranov <lugatex@yahoo.com>
2019-08-24 20:07:20 +0300

    added packages and setup config

---
+++ config/config.exs
@@ -0,0 +1,10 @@
+use Mix.Config
+
+if Mix.env == :dev do
+  config :mix_test_watch, clear: true
+  config :remix, escript: true, silent: true
+end
+
+if Mix.env == :test do
+  config :ex_unit_notifier, notifier: ExUnitNotifier.Notifiers.NotifySend
+end
